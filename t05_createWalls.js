6918f35b36d0442bf7b17520d4b26a7bc6befe60
23047fs <23047fs@hvhs.school.nz>
2026-02-09 13:23:37 +1300

    save

--- t05_createWalls.js
+++ t05_createWalls.js
@@ -26,22 +26,22 @@ function setup() {
 	*/
 
 	//Platform
-	platform_1 = new Sprite(1800, 450, 900, 10, 'k');
+	platform_1 = new Sprite(900, 0, 900, 10, 'k');
 	platform_1.rotation = 0;
 	platform_1.color = '#9e49bf';
 	platform_1.bounciness = 1;
 
-	platform_2 = new Sprite(0, 450, 900, 10, 'k');
+	platform_2 = new Sprite(900, 1800, 900, 10, 'k');
 	platform_2.rotation = 0;
 	platform_2.color = '#9e49bf';
 	platform_2.bounciness = 1;
 
-	platform_3 = new Sprite(900, 0, 900, 10, 'k');
+	platform_3 = new Sprite(0, 450, 900, 10, 'k');
 	platform_3.rotation = 90;
 	platform_3.color = '#9e49bf';
 	platform_3.bounciness = 1;
 
-	platform_4 = new Sprite(900, 900, 900, 10, 'k');
+	platform_4 = new Sprite(1800, 450, 900, 10, 'k');
 	platform_4.rotation = 90;
 	platform_4.color = '#9e49bf';
 	platform_4.bounciness = 1;
