b63adad423879684fd43d01c5febf0120dcc7824
23047fs <23047fs@hvhs.school.nz>
2026-02-09 13:28:56 +1300

    Monday Week 3

--- t05_createWalls.js
+++ t05_createWalls.js
@@ -29,12 +29,12 @@ function setup() {
 	platform_top = new Sprite(900, 0, 1800, 10, 'k');
 	platform_top.rotation = 0;
 	platform_top.color = '#fc0000';
-	platform_top.bounciness = 10;
+	platform_top.bounciness = 1;
 
 	platform_bottom = new Sprite(900, 900, 1800, 10, 'k');
 	platform_bottom.rotation = 0;
 	platform_bottom.color = '#b700ff';
-	platform_bottom.bounciness = 40;
+	platform_bottom.bounciness = 4;
 
 	platform_left = new Sprite(0, 450, 900, 10, 'k');
 	platform_left.rotation = 90;
@@ -44,7 +44,7 @@ function setup() {
 	platform_right = new Sprite(1800, 450, 900, 10, 'k');
 	platform_right.rotation = 90;
 	platform_right.color = '#3700ff';
-	platform_right.bounciness = 20;
+	platform_right.bounciness = 2;
 }
 	
 /*******************************************************/
