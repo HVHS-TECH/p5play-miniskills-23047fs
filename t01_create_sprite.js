b686229ff1858fd52fcc970d3215eb79499948d0
23047fs <23047fs@hvhs.school.nz>
2026-02-09 13:08:31 +1300

    save

--- t01_create_sprite.js
+++ t01_create_sprite.js
@@ -11,9 +11,10 @@ function setup() {
 	//Setup
 	console.log("setup: t01");
 	cnv = new Canvas(1800, 900);
+	world.gravity.y = 10;
 
 	//Rectangle
-	Rectangle = new Sprite(900, 450, 100, 100);
+	Rectangle = new Sprite(900, 450, 100, 100, 'd');
 	Rectangle.color = '#03fca5';
 	Rectangle.rotationSpeed = 2;
 	Rectangle.vel.x = 2;
