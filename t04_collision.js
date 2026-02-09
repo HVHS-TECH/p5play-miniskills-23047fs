3c4319795e89de0c3f09c0b0c91ab66da70f34c7
23047fs <23047fs@hvhs.school.nz>
2026-02-09 13:13:19 +1300

    save

--- t04_collision.js
+++ t04_collision.js
@@ -8,15 +8,32 @@
 // setup()
 /*******************************************************/
 function setup() {
-	console.log("setup: ");
+	//Setup
+	console.log("setup: t04");
+	cnv = new Canvas(1800, 900);
+	world.gravity.y = 10;
 
+	//Rectangle
+	Rectangle = new Sprite(900, 450, 100, 100, 'd');
+	Rectangle.color = '#03fca5';
+	Rectangle.rotationSpeed = 2;
+	Rectangle.vel.x = 2;
+
+	/*
+	//Circle
+	Circle = new Sprite(900, 175, 100, 's');
+	Circle.color = '#03fca5';
+	*/
+
+	//Platform
+	platform_1 = new Sprite(900, 300, 100, 10, 'k');
 }
 	
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-	
+	background('#b8b8b8'); 
 }
 
 /*******************************************************/
