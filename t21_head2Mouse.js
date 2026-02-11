d8fe9386a0ffc47ce48cfe252548c479f1b09f4f
23047fs <23047fs@hvhs.school.nz>
2026-02-12 11:00:08 +1300

    Thursday Week 3

--- t21_head2Mouse.js
+++ t21_head2Mouse.js
@@ -1,14 +1,24 @@
 /*******************************************************/
 // P5.play: t21_head2Mouse
 // Move sprite towards the mouse' position
-// Written by ???
+// Written by finley
 /*******************************************************/
 	
 /*******************************************************/
 // setup()
 /*******************************************************/
 function setup() {
-	console.log("setup: ");
+	//Setup
+	console.log("setup: 21");
+	cnv = new Canvas(1800, 900);
+	world.gravity.y = 5;
+
+	//Rectangle
+	rectangle = new Sprite(900, 450, 100, 100, 'd');
+	rectangle.color = '#03fca5';
+	rectangle.rotationSpeed = 2;
+	rectangle.vel.x = 0.1;
+	rectangle.vel.y = 0.1;
 
 }
 	
@@ -16,7 +26,12 @@ function setup() {
 // draw()
 /*******************************************************/
 function draw() {
-	
+	background('#b8b8b8'); 
+
+	//rectangle.moveTowards(mouseX, mouseY, 0.02);
+	if (mouse.presses()) {
+		rectangle.moveTo(mouseX, mouseY, 10);
+	}
 }
 
 /*******************************************************/
