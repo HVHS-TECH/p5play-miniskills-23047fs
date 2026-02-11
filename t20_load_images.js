4e977b40ad7ebe1b5668c3e9d9c92e40a2fa40fd
23047fs <23047fs@hvhs.school.nz>
2026-02-12 10:39:28 +1300

    save

--- t20_load_images.js
+++ t20_load_images.js
@@ -1,14 +1,27 @@
 /*******************************************************/
 // P5.play: t20_load_images
 // load & display images
-// Written by ???
+// Written by finley
 /*******************************************************/
 	
 /*******************************************************/
 // setup()
 /*******************************************************/
+function preload() {
+  imgBG   = loadImage('assets/images/space.jpg');
+  imgFace = loadImage('assets/images/face.png');
+
+}
+
 function setup() {
 	console.log("setup: ");
+	cnv = new Canvas(1800, 900);
+
+	ball = new Sprite(900, 450, 50, 'd');
+	ball.bounciness = 1;
+	ball.friction   = 0;
+	ball.image = (imgFace);
+	imgFace.resize(50, 50);
 
 }
 	
@@ -16,7 +29,7 @@ function setup() {
 // draw()
 /*******************************************************/
 function draw() {
-	
+	background(imgBG);
 }
 
 /*******************************************************/
