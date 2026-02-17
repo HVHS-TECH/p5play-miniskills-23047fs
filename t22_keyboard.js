565673bd4edfa5b0353f7c408a7821f5e984f62f
23047fs <23047fs@hvhs.school.nz>
2026-02-17 14:58:07 +1300

    save

--- t22_keyboard.js
+++ t22_keyboard.js
@@ -11,11 +11,26 @@ function setup() {
 	//Setup
 	console.log("setup: 22");
 	cnv = new Canvas(1800, 900);
-	//world.gravity.y = 10;
+	world.gravity.y = 23;
 
 	//Rectangle
 	rectangle = new Sprite(900, 450, 100, 100, 'd');
 	rectangle.color = '#03fca5';
+
+	//Ground
+	let isOnFloor = 1;
+
+	groundGroup = new Group();
+
+	platform = new Sprite(1400, 500, 1000, 10, 'k');
+	platform.color = '#6a3899';
+	platform.rotation = 315;
+	groundGroup.add(platform);
+
+	floor = new Sprite(900, 875, 1850, 50, 'k');
+	floor.color = '#ee4410';
+	floor.bounciness = 0.5;
+	groundGroup.add(floor);
 
 }
 	
@@ -26,47 +41,37 @@ function draw() {
 	background('#b8b8b8'); 
 
 	//Text
-	text("Hello World", 50, 50, 2000, 2000) 
+	text(round(rectangle.y), 900, 450);
 
 	//Keyboard Movement
-	rectangle.vel.x = rectangle.vel.x/1.025;
-	rectangle.vel.y = rectangle.vel.y/1.025;
-
-	if (kb.pressing('a')) {
-		// Set sprite's velocity to the left
-		rectangle.vel.x = -5;
-	};
-	if (kb.pressing ('d')) {
-		// Set sprite's velocity to the right
-		rectangle.vel.x = 5;
-	};
-	if (kb.pressing ('w')) {
-		// Set sprite's velocity to the up
-		rectangle.vel.y = -5;
-	};
-	if (kb.pressing ('s')) {
-		// Set sprite's velocity to the down
-		rectangle.vel.y = 5;
-	};
-
-	//Keyboard Rotation
+	rectangle.vel.x = rectangle.vel.x/1.04;
 	rectangle.rotationSpeed = rectangle.rotationSpeed/1.03;
 
-	//Rotate rectangle
-	if (kb.pressing ('q')) {
-		// Set sprite's rotation to the left
-		rectangle.rotationSpeed = -1.5;
+	//Jump
+	if (groundGroup.isTouching(rectangle)) {
+		isOnFloor = 1;
+	} else {
+		isOnFloor = 0;
 	};
-	if (kb.pressing ('e')) {
-		// Set sprite's rotation to the left
-		rectangle.rotationSpeed = 1.5;
+	if (kb.pressing ('w') && isOnFloor == 1) {
+		// Set sprite's velocity to the up
+		rectangle.vel.y = -15;
+		isOnFloor = 0;
+	};
+	
+	//Left
+	if (kb.pressing('a')) {
+		// Set sprite's velocity to the left
+		rectangle.vel.x = -4;
+		rectangle.rotationSpeed = -2.8;
 	};
 
-	//Set rectangle straight
-	if (kb.pressing ('f')) {
-		rectangle.rotationSpeed = 0;
-		rectangle.rotation = 0;
-	} 
+	//Right
+	if (kb.pressing ('d')) {
+		// Set sprite's velocity to the right
+		rectangle.vel.x = 4;
+		rectangle.rotationSpeed = 2.8;
+	};
 
 }
 
